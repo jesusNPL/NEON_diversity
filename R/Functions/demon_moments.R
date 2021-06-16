ab472084ab48d5d256b0913bc965f0e1c563c44d
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2021-06-16 11:32:44 -0400

    adding taxonomy

--- R/Functions/demon_moments.R
+++ R/Functions/demon_moments.R
@@ -62,9 +62,16 @@ demon_momentsTraitPhylo <- function(comm, trait, phylo, plotNames, nPlots) {
   require(moments)
   require(picante)
   
-  comm <- as.matrix(comm)
+  ## Store calculations taxonomy
+  meanTaxo <- numeric(length = nPlots)
+  medianTaxo <- numeric(length = nPlots)
+  sdTaxo <- numeric(length = nPlots)
+  varTaxo <- numeric(length = nPlots)
   
-  ## Store calculations 
+  skewTaxo <- numeric(length = nPlots)
+  kurtTaxo <- numeric(length = nPlots)
+  
+  ## Store calculations trait
   meanTrait <- numeric(length = nPlots)
   medianTrait <- numeric(length = nPlots)
   sdTrait <- numeric(length = nPlots)
@@ -73,7 +80,7 @@ demon_momentsTraitPhylo <- function(comm, trait, phylo, plotNames, nPlots) {
   skewTrait <- numeric(length = nPlots)
   kurtTrait <- numeric(length = nPlots)
   
-  ## Store calculations 
+  ## Store calculations phylogeny
   phyDat <- picante::evol.distinct(tree = phylo, type = "equal.splits")
   
   meanPhylo <- numeric(length = nPlots)
@@ -85,13 +92,28 @@ demon_momentsTraitPhylo <- function(comm, trait, phylo, plotNames, nPlots) {
   kurtPhylo <- numeric(length = nPlots)
   
   ## Start calculations
+  comm2 <- as.matrix(comm)
   
   for(i in 1:nPlots) {
-    spNames <- comm[i, ] > 0 
+    spNames <- comm2[i, ] > 0 
     selection <- names(spNames[spNames == TRUE])
     
     trait_comm <- trait[trait$Species %in% selection, ]
     phylo_comm <- phyDat[phyDat$Species %in% selection, ]
+    
+    ## Taxonomic moments
+    comm_tmp <- t(comm[i, ])
+    colnames(comm_tmp) <- "Abun"
+    
+    comm_tmp <- comm_tmp[rownames(comm_tmp) %in% selection, ]
+    
+    meanTaxo[i] <- mean(na.omit(comm_tmp))
+    medianTaxo[i] <- median(na.omit(comm_tmp))
+    sdTaxo[i] <- sd(na.omit(comm_tmp))
+    varTaxo[i] <- var(na.omit(comm_tmp))
+    
+    skewTaxo[i] <- skewness(na.omit(comm_tmp))
+    kurtTaxo[i] <- kurtosis(na.omit(comm_tmp))
     
     ## Trait moments
     meanTrait[i] <- mean(na.omit(trait_comm[, 2]))
@@ -109,17 +131,25 @@ demon_momentsTraitPhylo <- function(comm, trait, phylo, plotNames, nPlots) {
     skewPhylo[i] <- skewness(na.omit(phylo_comm[, 2]))
     kurtPhylo[i] <- kurtosis(na.omit(phylo_comm[, 2]))
   } 
+  # Taxonomy
+  momentsTaxo <- data.frame(plotNames, meanTaxo, medianTaxo, sdTaxo, varTaxo, 
+                              skewTaxo, kurtTaxo) 
+  names(momentsTaxo) <- c("plotID", "mean_Taxo", "median_Taxo", "sd_Taxo", 
+                            "var_Taxo", "skew_Taxo", "kurt_Taxo")
+  # Trait
   momentsTraits <- data.frame(plotNames, meanTrait, medianTrait, sdTrait, varTrait, 
                             skewTrait, kurtTrait) 
   names(momentsTraits) <- c("plotID", "mean_Trait", "median_Trait", "sd_Trait", 
                           "var_Trait", "skew_Trait", "kurt_Trait")
-  
+  # Phylogeny
   momentsPhylo <- data.frame(plotNames, meanPhylo, medianPhylo, sdPhylo, varPhylo, 
                               skewPhylo, kurtPhylo) 
   names(momentsPhylo) <- c("plotID", "mean_Phylo", "median_Phylo", "sd_Phylo", 
                             "var_Phylo", "skew_Phylo", "kurt_Phylo")
   
-  momentos <- list(moments_traits = momentsTraits, moments_phylo = momentsPhylo)
+  momentos <- list(moments_taxo = momentsTaxo, 
+                   moments_traits = momentsTraits, 
+                   moments_phylo = momentsPhylo)
   return(momentos)
 
 }
--- R/runMoments.R
+++ R/runMoments.R
@@ -1,12 +1,12 @@
 library(dplyr)
 library(tidyr)
+source("R/NEON_diversity/R/Functions/demon_moments.R")
 
 load("DATA/RData/HARV_SS.RData") # Spectral species
 ## Spectra data range from column 4 to 430
 plotNames <- unique(ss_threshold$plotID)
 nPlots <- length(plotNames)
 Range <- 5:430 ## Spectra data range from column 5 to 430
-
 
 x <- demon_momentsSPEC(spectra = ss_threshold, nPlots = nPlots, 
                        plotNames = plotNames, specRange = Range)
@@ -22,5 +22,6 @@ xx <- demon_momentsTraitPhylo(comm = matchPhylo$comm,
                               phylo = matchPhylo$phy, 
                               plotNames = plotNames, 
                               nPlots = nPlots)
+xx$moments_taxo
 xx$moments_traits
 xx$moments_phylo
